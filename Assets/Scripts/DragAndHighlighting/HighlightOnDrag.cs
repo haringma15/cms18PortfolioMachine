4ab93d2cbe84a356bba211ac82ef4aecf98aa1cf
haringma15 <martin.haring2@edu.fh-joanneum.at>
2020-01-07 01:37:52 +0100

    Optimize highlighting on drag logic

--- Assets/Scripts/DragAndHighlighting/HighlightOnDrag.cs
+++ Assets/Scripts/DragAndHighlighting/HighlightOnDrag.cs
@@ -1,22 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 // Adds color to all gameobjects with the 'area' tag when Mapsl is dragged.
 public class HighlightOnDrag : MonoBehaviour
 {
     public Color highlightingColor = Color.blue;
-    private GameObject[] areasToHighlight;
+    private GameObject[] allAreas;
+    private List<GameObject> areas = new List<GameObject>();
+    private bool isAreasSet = false;
     private bool isHighlighted = false;
 
-    void Awake() => areasToHighlight = GameObject.FindGameObjectsWithTag("area");
+    void Awake() => allAreas = GameObject.FindGameObjectsWithTag("area");
 
     void Update() {
-        if (PlayerPrefs.GetInt("isMapslDragged") == 1 && !isHighlighted) {
-            foreach (var area in areasToHighlight) changeColor(area, highlightingColor);
+        if (PlayerPrefs.GetInt("isMapslDragged") == 1 && !isAreasSet) {
+            foreach (GameObject go in allAreas) if (PlayerPrefs.GetString("island") == "" && go.name.Contains("Island") || PlayerPrefs.GetString("island") != "" && !go.name.Contains("Island")) areas.Add(go);
+            isAreasSet = true;
+        }
+        if (PlayerPrefs.GetInt("isMapslDragged") == 1 && !isHighlighted && isAreasSet) {
+            foreach (var area in areas) changeColor(area, highlightingColor);
             isHighlighted = true;
         }
         if (PlayerPrefs.GetInt("isMapslDragged") == 0 && isHighlighted) {
-            foreach (var area in areasToHighlight) changeColor(area, highlightingColor * -1);
+            foreach (var area in areas) changeColor(area, highlightingColor * -1);
             isHighlighted = false;
+            areas.Clear();
+            isAreasSet = false;
         }
     }
 
