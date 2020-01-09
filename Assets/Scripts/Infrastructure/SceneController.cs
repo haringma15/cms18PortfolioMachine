dfde46078210e6814120aa43a085f8426a3766a9
haringma15 <martin.haring2@edu.fh-joanneum.at>
2020-01-09 12:32:52 +0100

    Implement scene controller

---
+++ Assets/Scripts/Infrastructure/SceneController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    void Update() {
+        if (Input.GetKey(KeyCode.R)) SceneManager.LoadScene("Main");
+    }
+}
