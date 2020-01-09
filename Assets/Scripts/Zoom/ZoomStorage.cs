289d4e01e4f2cfd1289a6c1edd93fc7548ccac6c
haringma15 <martin.haring2@edu.fh-joanneum.at>
2020-01-10 00:47:15 +0100

    Adjust zoom positions

--- Assets/Scripts/Zoom/ZoomStorage.cs
+++ Assets/Scripts/Zoom/ZoomStorage.cs
@@ -10,8 +10,8 @@ public class ZoomStorage : MonoBehaviour
 
     private void fillZoomPositions() {
         zoomPositions.Add("Init", new Vector3(0, 0, -35));
-        zoomPositions.Add("InteractionIsland", new Vector3(-9.1f, 18.1f, 5));
-        zoomPositions.Add("CommunicationIsland", new Vector3(3.75f, -7.5f, -10));
+        zoomPositions.Add("InteractionIsland", new Vector3(-9, 18, 2));
+        zoomPositions.Add("CommunicationIsland", new Vector3(3.75f, -7.5f, -12));
         zoomPositions.Add("SoundIsland", new Vector3(-20.4f, -1, -10));
         zoomPositions.Add("MediaIsland", new Vector3(22.5f, 7.5f, -2));
     }
