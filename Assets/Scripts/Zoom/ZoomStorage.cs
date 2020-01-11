bb24136cc4726b3953bc4d1adbe5228b6c561538
haringma15 <martin.haring2@edu.fh-joanneum.at>
2020-01-11 22:27:55 +0100

    Finalize sound and center island

--- Assets/Scripts/Zoom/ZoomStorage.cs
+++ Assets/Scripts/Zoom/ZoomStorage.cs
@@ -12,7 +12,7 @@ public class ZoomStorage : MonoBehaviour
         zoomPositions.Add("Init", new Vector3(0, 0, -35));
         zoomPositions.Add("InteractionIsland", new Vector3(-11.9f, 17.8f, 2));
         zoomPositions.Add("CommunicationIsland", new Vector3(4f, -7.3f, -9));
-        zoomPositions.Add("SoundIsland", new Vector3(-20.4f, -1, -10));
+        zoomPositions.Add("SoundIsland", new Vector3(-20.2f, -1.3f, -10));
         zoomPositions.Add("MediaIsland", new Vector3(23.2f, 7.7f, -2));
     }
 
