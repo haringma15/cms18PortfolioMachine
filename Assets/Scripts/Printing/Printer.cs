4d3572a1a5524bbe89b06b579281f560747a469a
haringma15 <martin.haring2@edu.fh-joanneum.at>
2020-01-13 01:24:53 +0100

    Implement actual printing functionality, add printables

--- Assets/Scripts/Printing/Printer.cs
+++ Assets/Scripts/Printing/Printer.cs
@@ -3,14 +3,8 @@
 public class Printer : MonoBehaviour {
 
     public string baseFilePath = "C:\\visitenkarten\\";
-    private string filePath = "";
 
     public PrintingTool printingTool;
 
-    public void print()
-    {
-        filePath = baseFilePath + PlayerPrefs.GetString("region") + ".pdf";
-        printingTool.CmdPrintThreaded(filePath);
-        printingTool.StartCheckIsPrintingDone();
-    }
+    public void print() => printingTool.CmdPrintThreaded(baseFilePath + PlayerPrefs.GetString("region") + ".pdf");
 }
