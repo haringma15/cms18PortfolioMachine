dc72ae4a0a09d66eba9bdee2ce167b748b3c0120
haringma15 <martin.haring2@edu.fh-joanneum.at>
2020-01-15 00:10:18 +0100

    Add mapsl bubbles

---
+++ Assets/Scripts/DragAndHighlighting/BubbleController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BubbleController : MonoBehaviour
+{
+    public GameObject[] bubbleParticleSystems;
+    public int burstTimeInFrames = 30;
+
+    private GameObject randomBubble;
+    private int burstTimer;
+
+    void Start() {
+        burstTimer = burstTimeInFrames;
+    }
+
+    void Update() {
+        burstTimer--;
+        if (burstTimer <= 0) {
+            randomBubble = bubbleParticleSystems[Random.Range(0, bubbleParticleSystems.Length)];
+            Instantiate(randomBubble, transform.position, Quaternion.identity);
+            burstTimer = burstTimeInFrames;
+        }
+    }
+}
