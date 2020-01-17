e5d7157b1165db5644e9aadde09908b5ec3d16b9
haringma15 <martin.haring2@edu.fh-joanneum.at>
2020-01-17 23:27:25 +0100

    Add video functionality

---
+++ Assets/Scripts/PortfolioManagement/VideoController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    public string index;
+    public PortfolioController portfolioController;
+    public RawImage vidView;
+
+    private VideoPlayer vid;
+    private AudioSource vidSound;
+    private string activeProjectIndex;
+    private bool playing = false;
+
+    void Start() {
+        vid = GetComponent<VideoPlayer>();
+        vidSound = GetComponent<AudioSource>();
+    }
+
+    void Update() {
+        activeProjectIndex = portfolioController.getIndex();
+        if (!vid.isPrepared) vid.Prepare();
+        
+        if (vid.isPrepared && !playing && activeProjectIndex == index) {
+            vidView.texture = vid.texture;
+            vid.Play();
+            playing = true;
+        }
+        
+        if (playing && activeProjectIndex != index) {
+            vid.Pause();
+            playing = false;
+        }
+    }
+}
