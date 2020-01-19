3681d8b66675320dc6ca329404f0b6b812bac2be
haringma15 <martin.haring2@edu.fh-joanneum.at>
2020-01-19 04:52:11 +0100

    Add thumbnail functionality

--- Assets/Scripts/PortfolioManagement/VideoController.cs
+++ Assets/Scripts/PortfolioManagement/VideoController.cs
@@ -7,17 +7,23 @@ public class VideoController : MonoBehaviour
     public string index;
     public PortfolioController portfolioController;
     public RawImage vidView;
+    public GameObject thumbnail = null;
+    public int thumbnailHideTime = 4;
 
     private VideoPlayer vid;
     private string activeProjectIndex;
     private bool playing = false;
+    private int thumbnailHideTimer = -1;
 
     void Start() => vid = GetComponent<VideoPlayer>();
 
     void Update() {
         activeProjectIndex = portfolioController.getIndex();
-        if (!vid.isPrepared) vid.Prepare();
-        
+        if (!vid.isPrepared) {
+            playing = false;
+            vid.Prepare();
+        }
+
         if (vid.isPrepared && !playing && activeProjectIndex == index) {
             vidView.texture = vid.texture;
             vid.Play();
@@ -27,6 +33,16 @@ public class VideoController : MonoBehaviour
         if (playing && activeProjectIndex != index) {
             vid.Pause();
             playing = false;
+        }
+
+        if (thumbnail != null) {
+            if (!vid.isPrepared && !thumbnail.activeInHierarchy) thumbnail.SetActive(true);
+            if (vid.isPrepared && thumbnail.activeInHierarchy && thumbnailHideTimer < 0) thumbnailHideTimer = thumbnailHideTime;
+        }
+
+        if (thumbnailHideTimer >= 0) {
+            if (thumbnailHideTimer == 0) thumbnail.SetActive(false);
+            thumbnailHideTimer--;
         }
     }
 }
