4258e06789ae8696e23d89ab36027a9a25286b56
haringma15 <martin.haring2@edu.fh-joanneum.at>
2020-01-21 11:13:32 +0100

    Implement overlay for interactive elements

---
+++ Assets/Scripts/PortfolioManagement/TabletOverlayController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class TabletOverlayController : MonoBehaviour
+{
+    public GameObject interactionOrMediaOverlay;
+    public GameObject soundOrCommunicationOverlay;
+
+    private bool isBackgroundSet;
+    
+    void Start() => disableAll();
+
+    void Update() {
+        if (PlayerPrefs.GetString("region") != "" && !isBackgroundSet) {
+            switch (PlayerPrefs.GetString("island")) {
+                case "InteractionIsland":
+                    interactionOrMediaOverlay.SetActive(true);
+                    soundOrCommunicationOverlay.SetActive(false);
+                    break;
+                case "SoundIsland":
+                    interactionOrMediaOverlay.SetActive(false);
+                    soundOrCommunicationOverlay.SetActive(true);
+                    break;
+                case "CommunicationIsland":
+                    interactionOrMediaOverlay.SetActive(false);
+                    soundOrCommunicationOverlay.SetActive(true);
+                    break;
+                case "MediaIsland":
+                    interactionOrMediaOverlay.SetActive(true);
+                    soundOrCommunicationOverlay.SetActive(false);
+                    break;
+            }
+            isBackgroundSet = true;
+        }
+    }
+
+    public void disableAll() {
+        isBackgroundSet = false;
+        interactionOrMediaOverlay.SetActive(false);
+        soundOrCommunicationOverlay.SetActive(false);
+    }
+}
